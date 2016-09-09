667a3df65f381a1aadfca6aee65975b683a05587
Mona Gupta <monagupta43@gmail.com>
2016-09-09 15:19:28 -0500

    update schema to enable extension plpgsql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160831020309) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "photos", force: :cascade do |t|
     t.string   "url"
     t.datetime "created_at", null: false
